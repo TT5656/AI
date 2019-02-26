23e60b7eda5e42c825b50726f55ee52432d091f5
Steven Wang <steven.wang@arm.com>
2019-02-26 18:46:18 +0800

    third commit

--- app/src/main/java/com/example/myapplication/Camera2Fragment.java
+++ app/src/main/java/com/example/myapplication/Camera2Fragment.java
@@ -1,23 +1,63 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Camera2Fragment extends Fragment {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
-     * @return A new instance of fragment Camera2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
+public class Camera2Fragment extends Fragment {
+
+    private CameraDevice mCameraDevice;
+
+    private String mCameraId;
+
+    private Semaphore mCameraOpenCloseLock = new Semaphore(1);
+
+    private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice) {
+            // This method is called when the camera is opened.  We start camera preview here.
+            mCameraOpenCloseLock.release();
+            mCameraDevice = cameraDevice;
+//            createCameraPreviewSession();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+            mCameraOpenCloseLock.release();
+            cameraDevice.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int error) {
+            mCameraOpenCloseLock.release();
+            cameraDevice.close();
+            mCameraDevice = null;
+            Activity activity = getActivity();
+            if (null != activity) {
+                activity.finish();
+            }
+        }
+
+    };
+
     public static Camera2Fragment newInstance() {
         return new Camera2Fragment();
     }
@@ -41,5 +81,30 @@ public class Camera2Fragment extends Fragment {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
             return;
         }
+        Activity activity = getActivity();
+        CameraManager manager = (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics characteristics
+                        = manager.getCameraCharacteristics(cameraId);
+        StreamConfigurationMap configs = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        int [] formats = configs.getOutputFormats();
+
+                if (CameraCharacteristics.LENS_FACING_FRONT == characteristics.get(CameraCharacteristics.LENS_FACING))
+                    continue;
+
+                mCameraId = cameraId;
+
+                if (!mCameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
+                    throw new RuntimeException("Time out waiting to lock camera opening.");
+                }
+                manager.openCamera(mCameraId, mStateCallback, null);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock camera opening.", e);
+        }
+
     }
 }
